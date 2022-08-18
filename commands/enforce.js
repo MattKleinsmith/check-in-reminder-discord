cbcb6baf0b8a714266b06748515eee2c54811ec2
Matt Kleinsmith <8968171+MattKleinsmith@users.noreply.github.com>
2022-08-18 11:11:34 -0500

    Add badbot response

--- commands/enforce.js
+++ commands/enforce.js
@@ -1,3 +1,5 @@
+const { MessageAttachment, Message } = require('discord.js');
+
 const initEnforce = function (client) {
 
     this.enforceKeywords = ["assessment", "asesment", "assessmeant", "accessment", "assessmetn", "assement", "asessment", "assesments", "asseessment", "assessmt", "assemsent", "assetment", "assestment", "assessemtn", "asssesment", "assessmnt", "assessemnt", "assessemt", "assesssment", "assessmsent", "assesement", "assesment", "assessement", "essesment", "assemment", "assessmetnt", "assessmet", "assesemnet", "assessent", "accesment", "assessmenst", "assesmet", "assessmsnt", "assessmant", "aseessment", "assssemant", "assisment", "assassment", "assessmen", "assessmemt", "asseesment", "asseement", "assesmnet", "assissment", "acessment", "assesstment", "asssessment", "assessmnet", "assessessment", "asessments", "assemenet", "essessment", "assesmetn", "assisments", "assescment", "assiment", "assment", "assessmnent", "assasment", "aassessment", "assesmsnt", "asscesment", "assesemsent", "assesmnent", "assessament", "assessmernt", "asssment"]
@@ -31,8 +33,12 @@ const initEnforce = function (client) {
     this.enforceReplies = [`To avoid a/A Code of Conduct violations, please use ${assessmentPrep} to discuss the assessment.`, oldReply]
 
     this.gigabrax = "<:gigabrax:1009607197547835573>"
+
     this.goodBot = "good bot"
     this.goodBotReply = ":D"
+
+    this.badBot = "bad bot"
+    this.badBotReply = "᲼"
 }
 
 const enforce = function (client) {
@@ -99,6 +105,25 @@ const enforce = function (client) {
         unrepliedTriggers.forEach(message => {
             console.log("Replying to", `"${message.content}"`, "by", message.author.username);
             message.reply(this.goodBotReply);
+        })
+    }))
+
+    this.memeChannels.forEach(channel => channel.messages.fetch({ limit: 10 }).then(messages => {
+        // Get recent messages that include a keyword
+        const allTriggers = messages.filter(message => message.content.toLowerCase().includes(this.badBot) && message.author.username != client.user.username);
+
+        // Get IDs of already-replied-to triggers
+        const alreadyRepliedIds = messages
+            .filter(message => message.attachments.size > 0 && message.reference)
+            .map(message => message.reference.messageId);
+
+        // Don't reply twice
+        const unrepliedTriggers = allTriggers.filter(message => !alreadyRepliedIds.includes(message.id))
+
+        // Reply to each
+        unrepliedTriggers.forEach(message => {
+            console.log("Replying to", `"${message.content}"`, "by", message.author.username);
+            message.reply({ files: [{ attachment: "./cache/bad-bot.png" }] })
         })
     }))
 
