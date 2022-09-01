b1a931a28ee0f2bc1b326eff01fef43ec4731259
Matt Kleinsmith <8968171+MattKleinsmith@users.noreply.github.com>
2022-09-01 10:22:44 -0500

    Create minimal branch. Check-in only

--- commands/remind.js
+++ commands/remind.js
@@ -6,8 +6,6 @@ const initRemind = function (client) {
         { start: "12:25:00 PM", end: "12:33:00 PM", seen: false, isReport: false },
         { start: "2:55:00 PM", end: "3:03:00 PM", seen: false, isReport: false },
 
-        // { start: "12:00:00 AM", end: "11:59:00 PM", seen: false, isReport: false },  // test
-
         { start: "6:00:00 PM", end: "7:00:00 PM", seen: false, isReport: true },  // Report
     ];
 
@@ -15,12 +13,10 @@ const initRemind = function (client) {
 
     this.today = new Date().toLocaleString('en-US', { timeZone: this.timezone, weekday: 'long' });  // Monday
 
-    this.reminderChannel = client.channels.cache.get("1001711778952130662");  // #general
-    // this.reminderChannel = client.channels.cache.get("985966960334491671");  // Test server
+    this.reminderChannel = client.channels.cache.get("1013850605619531806");  // #general
 
     this.cohortGuild = client.guilds.cache.get("1001711778490744884");
-    this.reminderRole = "1002792225727598644";  // Codebusters role
-    this.friendRole = "1006573434161082508";  // Friend role
+    this.reminderRole = "1013844230269063240";  // Codebusters role
 
     console.table(this.checkIns);
 }
@@ -29,8 +25,6 @@ const remind = function () {
     const dayOfWeek = new Date().toLocaleString('en-US', { timeZone: this.timezone, weekday: 'long' })  // "Monday"
     const fullDate = new Date().toLocaleString('en-US', { timeZone: this.timezone })  // '8/10/2022, 4:19:54 PM'
     const date = fullDate.split(',')[0]  // '8/10/2022'
-
-    // console.log("Checking time:", dayOfWeek, fullDate, "PST")
 
     // Reset check-in reminders each day
     if (dayOfWeek !== this.today) {
@@ -49,13 +43,12 @@ const remind = function () {
         const start = date + ", " + checkIn.start;
         const end = date + ", " + checkIn.end;
         if (!checkIn.seen && new Date(fullDate) >= new Date(start) && new Date(fullDate) < new Date(end)) {
-            console.log("Sending reminder");
+            console.log("Sending reminder", fullDate);
 
             if (checkIn.isReport) {
-                // TODO: Do not follow up. Just send a message to that channel.
-                this.reminderChannel.send(`<@&${this.reminderRole}> <@&${this.friendRole}> Don't forget to fill out the daily report: https://progress.appacademy.io/`);
+                this.reminderChannel.send(`<@&${this.reminderRole}> Don't forget to fill out the daily report: https://progress.appacademy.io/`);
             } else {
-                this.reminderChannel.send(`<@&${this.reminderRole}> <@&${this.friendRole}> Don't forget to check in: https://progress.appacademy.io/`);
+                this.reminderChannel.send(`<@&${this.reminderRole}> Don't forget to check in: https://progress.appacademy.io/`);
             }
             checkIn.seen = true;
         }
--- index.js
+++ index.js
@@ -1,9 +1,6 @@
 const { Client, GatewayIntentBits } = require('discord.js');
 const { token } = require('./config.json');
 const { initRemind, remind } = require('./commands/remind.js');
-const { announce } = require('./commands/announce.js');
-const { initEnforce, enforce } = require('./commands/enforce.js');
-const { test } = require('./commands/test.js');
 
 const client = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildVoiceStates] });
 
@@ -13,26 +10,7 @@ client.once('ready', () => {
     initRemind.call(this, client);
     setInterval(remind.bind(this), 30 * 1000)
 
-    initEnforce.call(this, client);
-    setInterval(enforce.bind(this, client), 2 * 1000);
-
     this.username = client.user.username;
-});
-
-client.on('voiceStateUpdate', announce.bind(this));
-client.on('interactionCreate', async interaction => {
-    if (!interaction.isChatInputCommand()) return;
-
-    const { commandName } = interaction;
-    console.log(commandName)
-
-    if (commandName === 'quietstart') {
-        await interaction.reply(".", { ephemeral: true });
-        await interaction.deleteReply();
-    }
-    else if (commandName === 'test') {
-        test.call(this, interaction);
-    }
 });
 
 client.on('shardError', error => {
